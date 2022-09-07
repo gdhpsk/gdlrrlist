0ce83058d954b4bad355697b2bd81c8de432b33e
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-09-07 04:03:00 +0000

    adding ML route

--- api/v1/demons.js
+++ api/v1/demons.js
@@ -2,8 +2,12 @@ const config = require("../config.json")
 const { default: mongoose } = require("mongoose")
 const allowedPeople = require("../../schemas/allowedPeople.js")
 const jwt = require("jsonwebtoken")
+const {request} = require("undici")
+const { JSDOM } = require("jsdom");
 const opinionSchema = require("../../schemas/opinions.js")
 const levelsSchema = require("../../schemas/levels.js")
+const dayjs = require("dayjs")
+let reg  = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/
 let routes = {}
 
 module.exports = (authFunction, webhook, rate_lim) => {
@@ -23,6 +27,64 @@ const router = express.Router()
 router.use(express.urlencoded({ extended: true }))
   router.use(authenticator)
 
+router.get("/ML", async (req, res) => {
+  let ok = await request("https://sites.google.com/view/gd-mobile-lists/top-100-demons-completed")
+  let html = await ok.body.text()
+  const dom = new JSDOM(html);
+  let obj = []
+  for(let j = 2; j < dom.window.document.getElementsByClassName("oKdM2c Kzv0Me").length; j++) {
+    let txt = ""
+  let level = dom.window.document.getElementsByClassName("oKdM2c Kzv0Me")[j].getElementsByClassName("CDt4Ke zfr3Q").item(0)
+  for(let i = 0; i < level.getElementsByTagName("span").length; i++) {
+    let item = level.getElementsByTagName("span").item(i)
+    txt += item.innerHTML
+  }
+ txt = JSDOM.fragment(txt).textContent.split("(~")[0]
+let g = {}
+g.name = txt.split(`"`)[1].trim()
+let level_i = await request(`https://pointercrate.com/api/v2/demons/?name=${g.name}`)
+let level_id = await level_i.body.json()
+if(level_id.length != 0) {
+  g.pointercrateID = level_id[0].id
+}
+g.creators =  {
+  host: txt.split("(")[0].split("by")[1].trim()
+}
+let recordarr = []
+let records = dom.window.document.getElementsByClassName("tyJCtd mGzaTb baZpAe")[(j*2)-1].getElementsByClassName("CDt4Ke zfr3Q")
+for(let x = 0; x < records.length; x++) {
+  let parts = JSDOM.fragment(records.item(x).innerHTML).textContent.trim().split(" ").filter(e => e != "")
+  let name = parts.filter(e => !e.toLowerCase().startsWith("https://") && e != "-" && !e.toLowerCase().includes("hz)") && !e.toLowerCase().includes("youtu.be")).reduce(
+    (previousValue, currentValue) => previousValue + " " + currentValue
+  ).replace("-", "").trim()
+  recordarr.push({
+    name,
+    link: parts.find(e => e.toLowerCase().includes("youtu") || e.toLowerCase().startsWith("https://"))
+  })
+  let daa = await request(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${reg.exec(recordarr[x].link)}&key=${process.env.ytkey}`)
+  let data = await daa.body.json()
+  if(data.items.length == 0) {
+    recordarr[x].uploadDate = dayjs().format("M/D/YYYY")
+  } else {
+    recordarr[x].uploadDate = dayjs(Date.parse(data.items[0].snippet.publishedAt)).format("M/D/YYYY")
+  }
+  if(!recordarr[x].link.startsWith("https://")) {
+    recordarr[x].link = `https://${recordarr[x].link}`
+  }
+  if(parts.find(e => e.toLowerCase().includes("hz)"))) {
+    recordarr[x].hertz =  parseInt(parts.find(e => e.toLowerCase().includes("hz)")).replace("(", ""))
+  } else {
+    recordarr[x].hertz = 60
+  }
+}
+g.records = recordarr
+obj.push(g)
+}
+  res.json(obj)
+// fs.writeFile("./smt.json", JSON.stringify(obj), function(e, d) {
+
+// })
+})
 
 router.get("/", async (req, res) => {
   let config = {
