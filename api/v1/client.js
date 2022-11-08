63befd5bafce91bd2413fcc7c5d038ee9f3f4e30
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-11-08 06:21:04 +0000

    adding rest

--- api/v1/client.js
+++ api/v1/client.js
@@ -10,6 +10,9 @@ const loginSchema = require("../../schemas/logins.js")
 const mailSchema = require("../../schemas/mail")
 const bcrypt = require("bcrypt")
 let routes = {}
+const {REST} = require("@discordjs/rest")
+const {Routes} = require("discord-api-types/v10")
+const rest = new REST({version: '10'}).setToken(discord_token);
 
 module.exports = (authFunction, webhook, rate_lim, send) => {
   let authenticator = async (req, res, next) => {
